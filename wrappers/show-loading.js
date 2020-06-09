fc0c930b103678b776e54c3b3e25c53d0cbb5ea1
Eric Conklin <eric.s.conklin@gmail.com>
2020-06-09 14:20:48 -0400

    stuff

--- wrappers/show-loading.js
+++ wrappers/show-loading.js
@@ -2,13 +2,29 @@ import React from "react"
 
 import { ScalableLoading } from "../components/Loading/LoadingPage"
 
-export default (Component, options = {}) =>
-  ({ loading, ...props }) =>
-    !loading ? <Component { ...props }/> :
-    <div>
+const DEFAULT_OPTIONS = {
+  display: "fixed", // or absolute
+  size: "screen", // or full
+  scale: 1
+}
+
+export default (Component, options = {}) => {
+  const OPTIONS = {
+    ...DEFAULT_OPTIONS,
+    ...options
+  }
+  return ({ ...props }) =>
+    <>
       <Component { ...props }/>
-      <div className="fixed left-0 top-0 w-screen h-screen z-50 flex justify-center items-center"
-        style={ { backgroundColor: "rgba(0, 0, 0, 0.5)" } }>
-        <ScalableLoading scale={ options.scale || 1 }/>
-      </div>
-    </div>
+      { !props.loading ? null :
+        <div className={ `
+            ${ OPTIONS.display } left-0 top-0
+            w-${ OPTIONS.size } h-${ OPTIONS.size } z-50
+            flex justify-center items-center
+          ` }
+          style={ { backgroundColor: "rgba(0, 0, 0, 0.5)" } }>
+          <ScalableLoading scale={ OPTIONS.scale } color={ OPTIONS.color }/>
+        </div>
+      }
+    </>
+}
