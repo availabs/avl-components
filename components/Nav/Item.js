e7ac92bb0d26834a25530cc82f3d047411629b8f
Eric Conklin <eric.s.conklin@gmail.com>
2020-06-15 10:14:42 -0400

    stuff

--- components/Nav/Item.js
+++ components/Nav/Item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 // import { classNames } from "../utils";
 import Icon from "../Icons";
 
@@ -11,18 +11,20 @@ export function classNames(...classes) {
 
 
 export default ({ to, icon, className, children, theme, type='side' }) => {
-  const { pathname } = useLocation();
-  let active = (pathname === to);
-
-  let sideClasses = active ? theme.sidebarItemActive : theme.sidebarItem
-  let topClasses = active ? theme.topnavItemActive : theme.topnavItem
-  let linkClasses = type === 'side' ? sideClasses : topClasses
+//   const { pathname } = useLocation();
+//   let active = false;//(to.includes(pathname));
+// console.log("PATH NAME:", pathname, to)
+//
+//   let sideClasses = active ? theme.sidebarItemActive : theme.sidebarItem
+//   let topClasses = active ? theme.topnavItemActive : theme.topnavItem
+  const linkClasses = type === 'side' ? theme.sidebarItem : theme.topnavItem,
+    activeClasses = type === 'side' ? theme.sidebarItemActive : theme.topnavItemActive;
 
   return (
-    <Link to={to} className={linkClasses}>
+    <NavLink to={to} className={linkClasses} activeClassName={ activeClasses }>
       <Icon icon={icon} className={theme.menuIcon} showBlank={ type === "side" }/>
       {children}
-    </Link>
+    </NavLink>
   );
 
 
