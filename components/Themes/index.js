28618d7ec8e606d63dec44a8c7cd6675b9c23450
alex <am3081@gmail.com>
2020-06-17 11:59:33 -0400

    no double

--- components/Themes/index.js
+++ components/Themes/index.js
@@ -42,9 +42,6 @@ export const flat = {
 	buttonPrimary: 'inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out',
 }
 
-
-
-
 export const dark = {
 	bg: 'bg-gray-300',
 	menuBg: 'bg-gray-800',
@@ -70,7 +67,7 @@ export const dark = {
 	buttonPrimary: 'inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out',
 }
 
-export const dark = {
+export const blue = {
 	bg: 'bg-gray-200',
 	menuBg: 'bg-indigo-800',
 	sidebarW: '56',
