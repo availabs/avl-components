73e77cac1fc422885dc65243756e7d6f43ae56cb
Eric Conklin <eric.s.conklin@gmail.com>
2020-06-17 17:46:38 -0400

    Stuff

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,14 +1,24 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-export const Button = ({ children, className = "", theme={}, disabled = false, type = "button", ...props }) =>
-  <button { ...props } type={ type } disabled={ disabled }
-    className={ `${theme[type]}`}>
-    { children }
-  </button>
+import { useTheme } from "../../wrappers/with-theme"
 
-export const LinkButton = ({ children, href, theme={}, disabled = false, type='button', ...props}) =>
-	<Link { ...props } disabled={ disabled } onClick={ e => e.stopPropagation() }
-    	className={`${theme[type]}`}>
+export const Button = ({ children, className = "", disabled = false, type = "button", ...props }) => {
+  const theme = useTheme();
+  return (
+    <button { ...props } type={ type } disabled={ disabled }
+      className={ `${ theme[type] } ${ className }` }>
+      { children }
+    </button>
+  )
+}
+
+export const LinkButton = ({ children, className = "", disabled = false, type='button', ...props}) => {
+  const theme = useTheme();
+  return (
+  	<Link { ...props } disabled={ disabled } onClick={ e => e.stopPropagation() }
+    	className={ ` ${ theme[type] }  ${ className }` }>
     	{ children }
   	</Link>
+  )
+}
