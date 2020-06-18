690a751b3889fefa6727248f7ad3dd8a3a5f533e
Eric Conklin <eric.s.conklin@gmail.com>
2020-06-18 13:36:46 -0400

    stuff

--- components/Themes/index.js
+++ components/Themes/index.js
@@ -1,11 +1,12 @@
 const button = [
-	{ default: "rounded inline-flex items-center justify-center transition duration-150 ease-in-out" }, // <-- applied to all buttons
+	{ default: "rounded inline-flex items-center justify-center transition duration-150 ease-in-out disabled:cursor-not-allowed" }, // <-- applied to all buttons
 	{ default: "button", // <-- this is pulled from the theme during composeDefaults
-		Primary: "buttonPrimary" // <-- this is pulled from the theme during composeDefaults
+		Primary: "buttonPrimary", // <-- this is pulled from the theme during composeDefaults
+
 	},
 	{ default: "py-1 px-4",
-		Large: "py-2 px-6",
-		Small: "py-0 px-2"
+		Large: "py-2 px-6 text-lg",
+		Small: "py-0 px-2 text-sm"
 	},
 	{ Block: "w-full" }
 ]
@@ -16,8 +17,30 @@ const compositions = {
 }
 
 const TEST_THEME_BASE = {
-	button: "border border-gray-300 bg-gray-100 hover:bg-gray-300",
-	buttonPrimary: "bg-blue-300 text-white hover:bg-blue-500",
+	bg: 'bg-gray-200',
+	shadow: 'shadow',
+	ySpace: 'py-4',
+	width: 'max-w-7xl mx-auto',
+	menuBg: 'bg-white',
+	sidebarW: '48',
+	sidebarBorder: 'border-r border-gray-200',
+	text: 'text-gray-800',
+	menuIcon: 'mr-3 h-6 w-6',
+	topnavItem: 'mr-4 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out',
+	topnavItemActive: 'mr-4 inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none hover:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out',
+	sidebarItem: 'mb-1 group flex pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out',
+	sidebarItemActive: 'mb-1 group flex pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 hover:bg-indigo-100 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out',
+	headerBg: 'bg-white',
+	headerShadow: 'shadow',
+	contentBg: 'bg-gray-100',
+	accent1: 'bg-gray-300',
+	accent2: 'bg-gray-400',
+	accent3: 'bg-gray-500',
+	lighter: 'bg-gray-50',
+
+	button: "border border-gray-300 bg-gray-100 hover:bg-gray-300 disabled:text-gray-400 disabled:bg-gray-100",
+	buttonPrimary: "bg-blue-500 text-white hover:bg-blue-700 disabled:opacity-50 disabled:bg-blue-500",
+
 	compositions
 }
 
@@ -65,11 +88,6 @@ const handler = {
 	}
 }
 export const TEST_THEME =  new Proxy(composeDefaults(TEST_THEME_BASE), handler);
-console.log("buttonSmall:", TEST_THEME.buttonSmall)
-console.log("buttonPrimaryBlock:", TEST_THEME.buttonPrimaryBlock)
-console.log("button:", TEST_THEME.button)
-TEST_THEME.test = "TESTING SET";
-console.log("TEST_THEME.test", TEST_THEME.test)
 
 export const light = {
 	bg: 'bg-gray-200',
