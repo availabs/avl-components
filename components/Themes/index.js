b29615daa679dc36c336849f22c14a01f845fe26
alex <am3081@gmail.com>
2020-07-06 09:47:15 -0400

    theme changes/

--- components/Themes/index.js
+++ components/Themes/index.js
@@ -49,6 +49,7 @@ const composeDefaults = theme => {
 	}
 	return composedTheme;
 }
+
 const handler = {
 	get: (theme, definition, receiver) => {
 		if (definition in theme) return theme[definition];
