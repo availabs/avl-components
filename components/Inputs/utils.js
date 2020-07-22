efea85d09e58e96587115ae420b62ea462b5954a
Eric Conklin <eric.s.conklin@gmail.com>
2020-07-22 17:22:49 -0400

    stuff

--- components/Inputs/utils.js
+++ components/Inputs/utils.js
@@ -1,31 +1,65 @@
 const DATE_REGEX = /^\d{4}-[01][0-9]-[0-3][0-9]$/,
   NUMBER_REGEX = /^(-(?=[1-9]|(0[.]0*[1-9]+)))?\d*[.]?\d+/;
 
-const PARENS_REGEX = /^([\[\(])(-?\d+),(-?\d+)([\)\]])$/,
-  COMPARATORS = [">", ">=", "<", "<="];
+const PARENS_REGEX = /^([[(])(-?\d+),(-?\d+)([)]])$/,
+  COMPARE_REGEX = /^([><]=?)(\d+)$/;
+
+const processArgs = (num, op, val) => {
+  switch (op) {
+    case ">":
+      return num > val;
+    case ">=":
+      return num >= val;
+    case "<":
+      return num < val;
+    case "<=":
+      return num <= val;
+    default:
+      return true;
+  }
+}
 
 const testNumber = (num, verify) => {
+  if (!verify) return true;
+
   verify = verify.replace(/\s/g, "");
 
-  const match = PARENS_REGEX.exec(verify);
-  if (match) {
-    let verified = true;
+  const args = [];
 
-    const [, open, min, max, close] = match;
+  const hasParens = PARENS_REGEX.exec(verify);
+  if (hasParens) {
+    const [, open, min, max, close] = hasParens;
     if (open === "(") {
-      verified = verified && (+num > min)
+      args.push(">", +min);
     }
-    else if (open === "[")
+    else if (open === "[") {
+      args.push(">=", +min);
+    }
+    if (close === ")") {
+      args.push("<", +max);
+    }
+    else if (close === "]") {
+      args.push("<=", +max);
+    }
   }
-  console.log("MATCH:", match)
+  const hasCompare = COMPARE_REGEX.exec(verify);
+  if (hasCompare) {
+    const [, op, value] = hasCompare;
+    args.push(op, +value);
+  }
+
+  let verified = true;
+  for (let i = 0; i < args.length; i += 2) {
+    verified = verified && processArgs(+num, ...args.slice(i, i + 2));
+  }
+  return verified;
 }
 
 export const verifyValue = (value, type, verify = null) => {
   switch (type) {
     case "number": {
 console.log("NUMBER VERIFY:", verify)
-      // Boolean(verify) && testNumber(value, verify);
-      return NUMBER_REGEX.test(value);
+      return NUMBER_REGEX.test(value) && testNumber(value, verify);
     }
     case "date": {
       const regex = ((typeof verify === "string") ? new RegExp(verify) : DATE_REGEX);
