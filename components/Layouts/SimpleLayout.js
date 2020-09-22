5011361c306810c870429d683e565694b5a9eeda
alex <am3081@gmail.com>
2020-09-22 09:38:12 -0400

    make simple simpler

--- components/Layouts/SimpleLayout.js
+++ components/Layouts/SimpleLayout.js
@@ -12,15 +12,9 @@ class Layout extends Component {
     const theme = this.props.theme;//themes[this.props.theme]
     return (
       <div className={`${theme.bg}`}>
-      	<div className={`min-h-screen ${this.props.maxWidth} mx-auto overflow-hidden`} >
-          <div className="flex h-full">
-            <div className="w-0 flex-1 overflow-hidden">
-              <main className={`flex-1 z-0 focus:outline-none min-h-screen h-full`}>
-                  { this.props.children }
-              </main>
-            </div>
-          </div>
-        </div>
+          <main className={``}>
+              { this.props.children }
+          </main>
       </div>
     )
   }
