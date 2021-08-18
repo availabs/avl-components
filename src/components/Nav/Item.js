d5e5fb95fbd684e5bb5e3526eb105baa458c7294
Alex Muro <am3081@gmail.com>
2021-08-18 10:24:17 -0400

    changes

--- src/components/Nav/Item.js
+++ src/components/Nav/Item.js
@@ -3,37 +3,46 @@ import { Link, useRouteMatch } from "react-router-dom";
 
 import Icon from "../Icons";
 
-import { useTheme } from "../../wrappers/with-theme"
+import { useTheme } from "../../wrappers/with-theme";
 
-import get from "lodash.get"
+import get from "lodash.get";
 
 const STYLES = {
 	side: {
-		top: "0%", left: "100%"
+		top: "0%",
+		left: "100%",
 	},
 	top: {
-		top: "100%", left: "50%",
-		transform: "translateX(-50%)"
-	}
-}
+		top: "100%",
+		left: "50%",
+		transform: "translateX(-50%)",
+	},
+};
 
-const NavItem = ({ to, icon, customTheme = {}, className = "", children, type='side', subMenus = [] }) => {
-
-	const theme = { ...useTheme(), ...customTheme };
+const NavItem = ({
+	children,
+	icon,
+	to,
+	onClick,
+	className = null,
+	type = "side",
+	active = false,
+	subMenus = [],
+}) => {
+	const theme = useTheme();
 
 	const To = React.useMemo(() => {
 		if (!Array.isArray(to)) {
 			return [to];
 		}
 		return to;
-	}, [to])
+	}, [to]);
 
 	const subTos = React.useMemo(() => {
 		const subs = subMenus.reduce((a, c) => {
 			if (Array.isArray(c.path)) {
 				a.push(...c.path);
-			}
-			else if (c.path) {
+			} else if (c.path) {
 				a.push(c.path);
 			}
 			return a;
@@ -43,59 +52,77 @@ const NavItem = ({ to, icon, customTheme = {}, className = "", children, type='s
 
 	const routeMatch = Boolean(useRouteMatch({ path: subTos, exact: true }));
 
-	const linkClasses = type === 'side' ? theme.navitemSide : theme.navitemTop,
-		activeClasses = type === 'side' ? theme.navitemSideActive : theme.navitemTopActive;
+	const linkClasses = type === "side" ? theme.navitemSide : theme.navitemTop;
+	const activeClasses =
+		type === "side" ? theme.navitemSideActive : theme.navitemTopActive;
 
-	const navClass = routeMatch ? activeClasses : linkClasses;
+	const navClass = routeMatch || active ? activeClasses : linkClasses;
 
 	const [showSubMenu, setShowSubMenu] = React.useState(false);
 
-  return (
-		<div className={ `
-				relative ${ type === "side" ? "block" : "flex" }
-			` }
-			style={ { alignItems: "stretch" } }
-			onMouseLeave={ e => setShowSubMenu(false) }
-			onMouseOver={ e => setShowSubMenu(true) }>
+	return (
+		<div
+			className={`
+				items-stretch ${type === "side" ? "block" : "flex"}
+			`}
+			onMouseLeave={(e) => setShowSubMenu(false)}
+			onMouseOver={(e) => setShowSubMenu(true)}
+		>
+			{to ? (
+				<Link to={To[0]} className={`${className ? className : navClass}`}>
+					{!icon ? null : (
+						<div className={`${className} flex justify-center items-center`}>
+							<span className={icon} />
+						</div>
+					)}
+					{children}
+				</Link>
+			) : (
+				<div
+					onClick={onClick ? onClick : () => {}}
+					className={`${className ? className : navClass}`}
+				>
+					{!icon ? null : (
+						<Icon
+							icon={icon}
+							className={theme.menuIcon || null}
+							showBlank={type === "side"}
+						/>
+					)}
 
-	    <Link to={ To[0] } className={ `${ className } ${ navClass }` }>
+					{children}
+				</div>
+			)}
 
-	      { !icon ? null :
-					<Icon icon={ icon }
-						className={ theme.menuIcon || null }
-						showBlank={ type === "side" }/>
-				}
-
-	      { children }
-
-	    </Link>
-
-			{ !showSubMenu || !subMenus.length ? null :
-				<div className={ `
+			{!showSubMenu || !subMenus.length ? null : (
+				<div
+					className={`
 						absolute flex
-						${ type === "side" ? `pl-1` : "h-10" }
-					` }
-					style={ STYLES[type] }>
-					<div className={ `
+						${type === "side" ? `pl-1` : "h-10"}
+					`}
+					style={STYLES[type]}
+				>
+					<div
+						className={`
 							flex whitespace-nowrap
-							${ type === "side" ? `flex-col` : "flex-row" }
-						` }
-						style={ {
-							minWidth: type === "top" ? null :
-								`${ +get(theme, "sidebarW", 64) * 0.25 }rem`
-						} }>
-
-						{ subMenus.map((sm, i) => (
-								<NavItem key={ i } to={ sm.path } icon={ sm.icon }
-									customTheme={ customTheme } type={ type }>
-									{ sm.name }
-								</NavItem>
-							))
-						}
+							${type === "side" ? "flex-col" : "flex-row"}
+						`}
+						style={{
+							minWidth:
+								type === "top"
+									? null
+									: `${+get(theme, "sidebarW", 64) * 0.25}rem`,
+						}}
+					>
+						{subMenus.map((sm, i) => (
+							<NavItem key={i} to={sm.path} icon={sm.icon} type={type}>
+								{sm.name}
+							</NavItem>
+						))}
 					</div>
 				</div>
-			}
+			)}
 		</div>
-  );
+	);
 };
 export default NavItem;
---
+++ src/components/Nav/Side.docs.js
@@ -0,0 +1,79 @@
+import SideNav from "./Side";
+
+export default {
+	Component: (props) => (
+		<div className="h-full w-full bg-gray-100">
+			<SideNav {...props} />
+		</div>
+	),
+	name: "Side Nav",
+	description: "A responsive verticle navigation component.",
+	props: [
+		{
+			name: "menuItems",
+			type: "data",
+			default: [
+				{
+					name: "Layouts",
+					className: "font-light text-xs uppercase pl-6 text-blue-500 py-3",
+				},
+				{
+					name: "Dashboard",
+					icon: "os-icon os-icon-layout",
+					active: true,
+				},
+				{
+					name: "Menu Styles",
+					icon: "os-icon os-icon-layers",
+				},
+				{
+					name: "Options",
+					className: "font-light text-xs uppercase pl-6 text-blue-500 py-3",
+				},
+				{
+					name: "Applications",
+					icon: "os-icon os-icon-package",
+				},
+				{
+					name: "Pages",
+					icon: "os-icon os-icon-file-text",
+				},
+				{
+					name: "UI Kit",
+					icon: "os-icon os-icon-life-buoy",
+				},
+				{
+					name: "Elements",
+					className: "font-light text-xs uppercase pl-6 text-blue-500 py-3",
+				},
+				{
+					name: "Emails",
+					icon: "os-icon os-icon-mail",
+				},
+				{
+					name: "Users",
+					icon: "os-icon os-icon-users",
+				},
+				{
+					name: "Forms",
+					icon: "os-icon os-icon-edit-32",
+				},
+				{
+					name: "Tables",
+					icon: "os-icon os-icon-grid",
+				},
+				{
+					name: "Tables",
+					icon: "os-icon os-icon-zap",
+				},
+			],
+		},
+	],
+	theme: ["sidebarWrapper"],
+	dependencies: [
+		{
+			name: "Nav Item",
+			theme: ["navitemSide", "navitemSideActive", "menuIcon", "menuIcon"],
+		},
+	],
+};
