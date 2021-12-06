60bca6714a59398b7bec0695867635c867b7be9f
Alex Muro <am3081@gmail.com>
2021-12-06 15:21:35 -0500

    adding examples to docs

--- src/components/Nav/Top.docs.js
+++ src/components/Nav/Top.docs.js
@@ -2,77 +2,81 @@ import TopNav from "./Top";
 //import { useTheme } from "../../wrappers";
 
 export default {
-	Component: (props) => {
-		return (
-			<div className="h-full w-full bg-gray-100">
-				<TopNav {...props} />
-			</div>
-		);
-	},
 	name: "Top Nav",
 	description: "A responsive horizontal navigation component.",
-	props: [
+	examples: [
 		{
-			name: "menuItems",
-			type: "data",
-			default: [
+			Component: (props) => {
+				return (
+					<div className="h-full w-full bg-gray-100">
+						<TopNav {...props} />
+					</div>
+				);
+			},
+			props: [
 				{
-					name: "Dashboard",
-					icon: "os-icon os-icon-layout",
-					active: true,
-				},
+					name: "menuItems",
+					type: "data",
+					default: [
+						{
+							name: "Hello",
+							icon: "os-icon os-icon-layout",
+							active: true,
+						},
 
-				{
-					name: "Applications",
-					icon: "os-icon os-icon-package",
-				},
-				{
-					name: "Pages",
-					icon: "os-icon os-icon-file-text",
-				},
+						{
+							name: "Applications",
+							icon: "os-icon os-icon-package",
+						},
+						{
+							name: "Pages",
+							icon: "os-icon os-icon-file-text",
+						},
 
-				{
-					name: "Emails",
-					icon: "os-icon os-icon-mail",
+						{
+							name: "Emails",
+							icon: "os-icon os-icon-mail",
+						},
+						{
+							name: "Users",
+							icon: "os-icon os-icon-users",
+						},
+						{
+							name: "Forms",
+							icon: "os-icon os-icon-edit-32",
+						},
+						{
+							name: "Tables",
+							icon: "os-icon os-icon-grid",
+						},
+					],
 				},
 				{
-					name: "Users",
-					icon: "os-icon os-icon-users",
+					name: "leftMenu",
+					type: "Component",
+					default: (
+						<div className="flex items-center p-4 justify-center h-12">
+							<span className="text-lg font-medium uppercase">AVL Design</span>
+						</div>
+					),
 				},
 				{
-					name: "Forms",
-					icon: "os-icon os-icon-edit-32",
-				},
-				{
-					name: "Tables",
-					icon: "os-icon os-icon-grid",
+					name: "rightMenu",
+					type: "Component",
+					default: (
+						<div className="flex items-center md:w-32 justify-center h-12 w-full hover:bg-gray-400 hover:text-white">
+							<span className="text-sm cursor-pointer">Login</span>
+						</div>
+					),
 				},
 			],
-		},
-		{
-			name: "leftMenu",
-			type: "Component",
-			default: (
-				<div className="flex items-center p-4 justify-center h-12">
-					<span className="text-lg font-medium uppercase">AVL Design</span>
-				</div>
-			),
-		},
-		{
-			name: "rightMenu",
-			type: "Component",
-			default: (
-				<div className="flex items-center md:w-32 justify-center h-12 w-full hover:bg-gray-400 hover:text-white">
-					<span className="text-sm cursor-pointer">Login</span>
-				</div>
-			),
-		},
-	],
-	theme: ["sidebarWrapper"],
-	dependencies: [
-		{
-			name: "Nav Item",
-			theme: ["navitemTop", "navitemTopActive", "menuIcon", "menuIcon"],
+			theme: ["sidebarWrapper"],
+			dependencies: [
+				{
+					name: "Nav Item",
+					theme: ["navitemTop", "navitemTopActive", "menuIcon", "menuIcon"],
+				},
+			],
 		},
 	],
 };
