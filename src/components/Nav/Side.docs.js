48b3937f7842ff4c9fcfd4531b22243a50d9e98f
shaunaksangdod <shaunaksangdod@gmail.com>
2022-04-29 13:10:09 -0400

    sidenav

--- src/components/Nav/Side.docs.js
+++ src/components/Nav/Side.docs.js
@@ -295,6 +295,121 @@ const SideNavDocs = {
         },
 
         {
+            title: 'Nested Menu - Always Show',
+            Component: (props) => (
+                <div className="h-full w-full bg-gray-100">
+                    <SideNav {...props} />
+                </div>
+            ),
+            props: [
+                {
+                    name: 'mobile',
+                    default: 'side'
+                },
+                {
+                    name: 'subMenuActivate',
+                    default: 'active' // onClick, onHover, active
+                },
+                {
+                    name: "menuItems",
+                    default: [
+                        {
+                            name: "Dashboard",
+                            icon: "os-icon os-icon-layout",
+                            active: true,
+                            subMenus: [
+                                {
+                                    name: "Menu with link",
+                                    icon: "os-icon os-icon-layers",
+                                    path: 'Table'
+                                },
+
+                                {
+                                    name: "Sub Menu 2",
+                                    icon: "os-icon os-icon-package",
+                                },
+                                {
+                                    name: "Sub Menu 3",
+                                    icon: "os-icon os-icon-file-text",
+                                },
+                            ]
+                        },
+                        {
+                            name: "Menu Styles",
+                            icon: "os-icon os-icon-layers",
+                            path: 'Table'
+                        },
+
+                        {
+                            name: "Applications",
+                            icon: "os-icon os-icon-package",
+                            subMenus: [
+                                {
+                                    name: "Sub Menu 1",
+                                    icon: "os-icon os-icon-layers",
+                                },
+
+                                {
+                                    name: "Sub Menu 2",
+                                    icon: "os-icon os-icon-package",
+                                },
+                                {
+                                    name: "Sub Menu 3",
+                                    icon: "os-icon os-icon-file-text",
+                                },
+                            ]
+                        },
+                        {
+                            name: "Pages",
+                            icon: "os-icon os-icon-file-text",
+                        },
+                        {
+                            name: "UI Kit",
+                            icon: "os-icon os-icon-life-buoy",
+                        },
+
+                        {
+                            name: "Emails",
+                            icon: "os-icon os-icon-mail",
+                        },
+                        {
+                            name: "Users",
+                            icon: "os-icon os-icon-users",
+                        },
+                        {
+                            name: "Forms",
+                            icon: "os-icon os-icon-edit-32",
+                        },
+                        {
+                            name: "Tables",
+                            icon: "os-icon os-icon-grid",
+                        },
+                        {
+                            name: "Tables",
+                            icon: "os-icon os-icon-zap",
+                        },
+                    ],
+                },
+                {
+                    name: "topMenu",
+                    default: (
+                        <div className="flex items-center p-6 justify-start h-12">
+                            <span className="text-lg font-medium uppercase">AVL Design</span>
+                        </div>
+                    )
+                },
+                {
+                    name: 'themeOptions',
+                    default: {
+                        color: 'white',
+                        size: 'compact',
+                        subMenuStyle: 'inline'
+                    }
+                }
+            ],
+        },
+
+        {
             title: 'Nested Menu - onHover',
             Component: (props) => (
                 <div className="h-full w-full bg-gray-100">
