d843efaf8727d4caeac927fa3d5124dbe898a94a
Alex Muro <am3081@gmail.com>
2022-09-26 12:03:12 -0400

    changes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -89,32 +89,21 @@ export const Button = ({
 };
 
 export const LinkButton = ({
-  buttonTheme = "button",
+  themeOptions={},
   className = "",
   type,
   children,
   disabled,
-  large,
-  small,
-  block,
-  active,
-  primary,
   ...props
 }) => {
   const theme = useTheme();
-  buttonTheme = `${buttonTheme}${composeOptions({
-    large,
-    small,
-    block,
-    disabled,
-    active,
-    primary
-  })}`;
+  const fullTheme = useTheme();
+  let buttonTheme = fullTheme['button'](themeOptions)
   return (
     <Link
       {...props}
       onClick={(e) => e.stopPropagation()}
-      className={` ${theme[buttonTheme] || theme["button"]} ${className}`}
+      className={`${buttonTheme} ${className}`}
     >
       {children}
     </Link>
