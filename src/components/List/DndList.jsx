03cec9d5f1a223a21f5cf17c071f0b981dbf0fab
Eric Conklin <eric.s.conklin@gmail.com>
2024-05-06 13:26:03 -0400

    stuff

--- src/components/List/DndList.jsx
+++ src/components/List/DndList.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'// 'react-beautiful-dnd';
 
 import { useTheme } from "../../wrappers/with-theme"
 
